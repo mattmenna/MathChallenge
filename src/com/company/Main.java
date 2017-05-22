622058d14ebc843bf8d195b36c095b897166a8b6
Matthew Menna <mattmenna@gmail.com>
2017-05-22 13:09:59 -0400

    Testing class for sum of cubes takes no input and only works on the number 153 and 154

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,18 +25,37 @@ public class Main {
         String numInput = "";
 
         System.out.println("Math Challenge Deliverable part 1");
-        System.out.println("Enter the interger your would like to check");
-        Scanner scrn = new Scanner(System.in);
-        numInput = scrn.nextLine();
-        System.out.println("You entered " + numInput);
+        System.out.println("Enter the interger your would like to check"); //Boilerplate Text
 
+        Scanner scrn = new Scanner(System.in); // Sets up to read from terminal
+        numInput = scrn.nextLine();
+
+
+        System.out.println("You entered " + numInput); // Verifies results - can add a yes no dialog later
+
+        // breaks up number into digits and lets user verify
         for (int i = 0; i < numInput.length(); i++) {
             System.out.println("Number at location " + i + " is " + numInput.charAt(i));
             System.out.println("_____________");
         }
+
         sumOfcubes op = new sumOfcubes();
 
-        op.checkSum(154, 1, 5, 4); // 153 is true 154 is false
+        int firstDigit = Character.getNumericValue(numInput.charAt(0));
+        int secondDigit = Character.getNumericValue(numInput.charAt(1));
+        int thirdDigit = Character.getNumericValue(numInput.charAt(2));
+
+        System.out.println("What was your number again?");
+        int sumOfdigits = scrn.nextInt();
+
+        System.out.println("First Digit is " + firstDigit);
+        System.out.println("Second Digit is " + secondDigit);
+        System.out.println("Third Digit is " + thirdDigit);
+
+        System.out.println("Total is "+ sumOfdigits);
+
+        System.out.println("Test CheckSum with integer");
+        op.checkSum(sumOfdigits, firstDigit, secondDigit, thirdDigit); // 153 is true 154 is false
 
 
     }
--- src/com/company/sumOfcubes.java
+++ src/com/company/sumOfcubes.java
@@ -4,10 +4,14 @@ package com.company;
  * Created by Matt on 5/22/2017.
  */
 public class sumOfcubes {
-    private int a = 0;
-    private int b = 0;
-    private int c = 0;
-    private int total = 0;
+
+    /**
+     *
+     * @param total
+     * @param a
+     * @param b
+     * @param c
+     */
 
     void checkSum(int total, int a, int b, int c){
         if(total == Math.pow(a,3)+ Math.pow(b,3) + Math.pow(c,3)){
@@ -18,5 +22,10 @@ public class sumOfcubes {
         }
     }
 
+    void checkString(String input){
+        for (int i = 0; i < input.length(); i++) {
+            input.charAt(i);
+        }
+    }
 
 }
