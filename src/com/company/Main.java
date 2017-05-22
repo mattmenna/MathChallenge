0c892412e8b7645a1050fb7abcfbaf96d41bd00a
Matthew Menna <mattmenna@gmail.com>
2017-05-22 10:13:57 -0400

    Scanner added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+
+    /*
+
+    For the input integer number, find if the sum of the cubes of the number's digits is equal to the number itself
+
+    Input: Program will read an integer number from the user.
+    Output: Your program should print out True or False based on the result.
+
+    Things to include
+    Proper validation on the user's input
+    Write the task in a seperate method
+    Provide adequate comments
+    Provide adequate user prompts.
+
+     */
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scrn = new Scanner(System.in);
+        System.out.println(scrn.nextLine());
+        
+
+
     }
 }
