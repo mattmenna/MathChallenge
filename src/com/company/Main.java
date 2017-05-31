a5e6a6a912383470386af9cdb2c11132f4304c1b
Matthew Menna <mattmenna@gmail.com>
2017-05-31 11:34:59 -0400

    Method work to take in array still needs to fix intInput

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,46 +21,37 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        String numInput = "";
+        String numInput;
 
         System.out.println("Math Challenge Deliverable part 1");
         System.out.println("Enter the interger your would like to check"); //Boilerplate Text
 
         Scanner scrn = new Scanner(System.in); // Sets up to read from terminal
-        numInput = scrn.nextLine();
+        numInput = scrn.nextLine(); //numInput is a string
 
+        int intInput = Integer.parseInt(numInput); //turns numInput into a int
+        int[] arrayInput;
 
+        arrayInput = new int[numInput.length()];
         System.out.println("You entered " + numInput); // Verifies results - can add a yes no dialog later
 
         // breaks up number into digits and lets user verify
         for (int i = 0; i < numInput.length(); i++) {
             System.out.println("Number at location " + i + " is " + numInput.charAt(i));
             System.out.println("_____________");
+            arrayInput[i]=intInput % 10;
+            System.out.println("Number at array location " + i + " is " + arrayInput[i]);
+            System.out.println("_____________");
+            intInput = intInput / 10;
         }
-
 
         sumOfcubes op = new sumOfcubes();
 
-        /*
-        int firstDigit = Character.getNumericValue(numInput.charAt(0));
-        int secondDigit = Character.getNumericValue(numInput.charAt(1));
-        int thirdDigit = Character.getNumericValue(numInput.charAt(2));
-        */
-
-        /*System.out.println("What was your number again?");
-        int sumOfdigits = scrn.nextInt();
-
-       System.out.println("First Digit is " + firstDigit);
-
-        System.out.println("Second Digit is " + secondDigit);
-        System.out.println("Third Digit is " + thirdDigit);
-
-        System.out.println("Total is "+ sumOfdigits);
-        */
         System.out.println("Testing CheckSum with integer");
-        op.checkSum(sumOfdigits, firstDigit, secondDigit, thirdDigit); // 153 is true 154 is false
-
-
+        if (intInput == op.checkSum(arrayInput)) {// 153 is true 154 is false
+            System.out.println("The integer is a perfect cube");
+        }
+        else { System.out.println("The integer is not a perfect cube");
+        }
     }
 }
--- src/com/company/sumOfcubes.java
+++ src/com/company/sumOfcubes.java
@@ -5,27 +5,16 @@ package com.company;
  */
 public class sumOfcubes {
 
-    /**
-     *
-     * @param total
-     * @param a
-     * @param b
-     * @param c
-     */
 
-    void checkSum(int total, int a, int b, int c){
-        if(total == Math.pow(a,3)+ Math.pow(b,3) + Math.pow(c,3)){
-    System.out.println("true");
-        }
-        else {
-            System.out.println("false");
-        }
+int checkSum(int[] inputNumberArray){
+    int total= 0;
+    for(int i = 0; i < inputNumberArray.length; i++){
+        total += Math.pow(inputNumberArray[i],3);
+        System.out.println("Total is " + total);
+    }
+    System.out.println("Final Total is " + total);
+    return total;
     }
 
-    void checkString(String input){
-        for (int i = 0; i < input.length(); i++) {
-            input.charAt(i);
-        }
-    }
 
 }
