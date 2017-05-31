ff34180943b7d010c4473ce96c264869d981fb13
Matthew Menna <mattmenna@gmail.com>
2017-05-31 11:46:04 -0400

    Method work to take in array still needs to fix intInput

--- src/com/company/sumOfcubes.java
+++ src/com/company/sumOfcubes.java
@@ -10,11 +10,28 @@ int checkSum(int[] inputNumberArray){
     int total= 0;
     for(int i = 0; i < inputNumberArray.length; i++){
         total += Math.pow(inputNumberArray[i],3);
-        System.out.println("Total is " + total);
+        //System.out.println("Total is " + total);
     }
-    System.out.println("Final Total is " + total);
+    //System.out.println("Final Total is " + total);
     return total;
     }
 
+int[] split(String numInput){
+    int intInput = Integer.parseInt(numInput); //turns numInput into a int
+    int[] arrayInput;
 
+    arrayInput = new int[numInput.length()];
+    System.out.println("You entered " + numInput); // Verifies results - can add a yes no dialog later
+
+    // breaks up number into digits and lets user verify
+    for (int i = 0; i < numInput.length(); i++) {
+        //System.out.println("Number at location " + i + " is " + numInput.charAt(i));
+        //System.out.println("_____________");
+        arrayInput[i]=intInput % 10;
+        //System.out.println("Number at array location " + i + " is " + arrayInput[i]);
+        //System.out.println("_____________");
+        intInput = intInput / 10;
+    }
+    return arrayInput;
+}
 }
